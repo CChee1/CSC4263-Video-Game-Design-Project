0ad53734de5c9c46512f6aac213d2bf0b09147f4
Pierre Gabory <piergabory@icloud.com>
2020-03-05 16:59:18 -0600

    bad controls and bad camers

--- Game/Assets/Scripts/TrainController.cs
+++ Game/Assets/Scripts/TrainController.cs
@@ -7,8 +7,11 @@ public class TrainController : MonoBehaviour
 
     public float sensitivity = 10f;
 
+    public float speed = 10f;
+
     public KeyCode moveLeftKey;
     public KeyCode moveRightKey;
+    public KeyCode moveForwardKey;
 
     // Start is called before the first frame update
     void Start()
@@ -23,12 +26,14 @@ public class TrainController : MonoBehaviour
 
         if (Input.GetKey(moveLeftKey)) {
             GetComponent<Rigidbody>().AddTorque(new Vector3(0, sensitivity, 0));
-            print("left!");
         }
         
         if (Input.GetKey(moveRightKey)) {
             GetComponent<Rigidbody>().AddTorque(new Vector3(0, -sensitivity, 0));
-            print("right!");
+        }
+
+        if (Input.GetKey(moveForwardKey)) {
+            GetComponent<Rigidbody>().AddRelativeForce(new Vector3(-speed, 0, 0));
         }
     }
 }
