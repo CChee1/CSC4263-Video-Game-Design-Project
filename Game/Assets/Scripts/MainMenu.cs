14154e125bcffb410fed147853637b8ce31b0608
Christopher Chee <cchee1@lsu.edu>
2020-04-02 02:20:52 -0500

    Functional main menu. Needs some polishing

---
+++ Game/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public bool isStart;
+    public bool isQuit;
+
+    private bool start = false;
+    private bool quit = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseUp()
+    {
+        if (isStart)
+        {
+            Debug.Log("Game started. Transition to next scene");
+            StartGame();
+        }
+        else if (isQuit)
+        {
+            Debug.Log("Quitting game.");
+            Application.Quit();
+        }
+    }
+
+    void StartGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
