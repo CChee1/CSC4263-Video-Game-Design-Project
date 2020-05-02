68854eee0fd583f8d9b58c3ba596e5c03b37ce1d
Christopher Chee <cchee1@lsu.edu>
2020-05-01 22:26:44 -0500

    Revert "Revert "Merge remote-tracking branch 'origin/master'""
    This reverts commit 8ce539288a69e71ab8bb6432a6f2cffd543403b3.

---
+++ Game/Assets/Scripts/MouseHover.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseHover : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<TextMesh>().color = Color.white;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseEnter()
+    {
+        GetComponent<TextMesh>().color = Color.yellow;
+    }
+
+    void OnMouseExit()
+    {
+        GetComponent<TextMesh>().color = Color.white;
+    }
+}
---
+++ Game/Assets/Scripts/Obstacle.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Obstacle {
+
+    void OnTriggerEnter(Collider col)
+    {
+        //if the player hits one obstacle, it's game over
+        if(col.gameObject.tag == Constants.PlayerTag)
+        {
+            GameManager.Instance.Die();
+        }
+    }
+}
